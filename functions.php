f00fa2e7bff479ad2e655b36059f8dbdfe6df064
Mike Haley <themichaelhaley@gmail.com>
2016-03-22 11:58:18 -0700

    configured sass, ready for build

--- functions.php
+++ functions.php
@@ -5,14 +5,14 @@ include_once( get_template_directory() . '/lib/init.php' );
 
 //* Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'Genesis Theme Demo 1' );
-define( 'CHILD_THEME_URL', 'http://localhost:8888' );
+define( 'CHILD_THEME_URL', 'http://www.hummingbirdesigns.com' );
 define( 'CHILD_THEME_VERSION', '2.2.2' );
 
 //* Enqueue Google Fonts
-//add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
-//function genesis_sample_google_fonts() {
-//wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
-//}
+add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
+function genesis_sample_google_fonts() {
+wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
+}
 
 //* Add HTML5 markup structure
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
@@ -22,6 +22,27 @@ add_theme_support( 'genesis-accessibility', array( 'headings', 'drop-down-menu',
 
 //* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
+
+//* Enqueue scripts and styles
+add_action( 'wp_enqueue_scripts', 'genesis_theme_demo_1_scripts_styles' );
+function genesis_theme_demo_1_scripts_styles() {
+	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(), 'genesis_theme_demo_1');
+}
+
+//* Add suppport for structural wraps
+add_theme_support( 'genesis-structural-wraps', array(
+	'header', 'nav', 'subnav', 'main', 'footer-widgets', 'footer',
+	) );
+
+//* Rename menus
+add_theme_support( 'genesis_menus', array(
+	'primary' => __( 'Header Top Navigation Menu', 'genesis_theme_demo_1'),
+	'secondary' => __( 'Header Bottom Navigation Menu', 'genesis_theme_demo_1')
+	) );
+
+//* Add new image sizes
+add_image_size( 'home-top', 780, 354, TRUE);
+add_image_size( 'home-middle', 375, 175, TRUE);
 
 //* Add support for custom background
 add_theme_support( 'custom-background');
